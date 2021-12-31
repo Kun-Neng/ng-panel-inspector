da5b3eb14778019f433fd2a13c9748c01ad189df
Kun-Neng <kunneng.hung@gmail.com>
2021-12-31 19:06:56 +0800

    add subscription to subscribe the selected defect and set the defect detail values

--- src/app/defect-details/defect-details.component.ts
+++ src/app/defect-details/defect-details.component.ts
@@ -1,22 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Defect } from '../interface/defect';
+import { MockDataService } from '../mock-data.service';
 
 @Component({
   selector: 'app-defect-details',
   templateUrl: './defect-details.component.html',
   styleUrls: ['./defect-details.component.css']
 })
-export class DefectDetailsComponent implements OnInit {
+export class DefectDetailsComponent implements OnInit, OnDestroy {
   autoTicks: boolean = false;
   showTicks: boolean = true;
   tickInterval: number = 1;
   max: number = 100;
   min: number = 10;
   step: number = 1;
-  severityValue = 10;
 
-  constructor() { }
+  defectDetails: Defect;
+
+  private defectSubscription: Subscription;
+
+  constructor(private mockDataService: MockDataService) {
+    this.defectDetails = {
+      uuid: '',
+      x: -1,
+      y: -1,
+      severity: -1,
+      isSelected: false
+    };
+
+    this.defectSubscription = this.mockDataService.$selectedDefectObservable.subscribe((defect: Defect) => {
+      if (defect.isSelected) {
+        this.defectDetails = {
+          uuid: defect.uuid,
+          x: defect.x,
+          y: defect.y,
+          severity: defect.severity,
+          isSelected: defect.isSelected
+        };
+      }
+    });
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    this.defectSubscription.unsubscribe();
   }
 
   getSliderTickInterval(): number | 'auto' {
