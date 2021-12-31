7ab61a6201a515d757b5553ef6fc1192ed547aaa
Kun-Neng <kunneng.hung@gmail.com>
2021-12-31 09:41:40 +0800

    add NO_ERRORS_SCHEMA to skip error message when testing Angular Material component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,6 +22,7 @@ import { DefectDetailsComponent } from './defect-details/defect-details.componen
     MatGridListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
