1fa420a9ba56d4aa8f6b9506c58f5f9363340b8a
Kun-Neng <kunneng.hung@gmail.com>
2021-12-31 15:32:47 +0800

    add subscription to subscribe the selected defect

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { MockDataService } from '../mock-data.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { Defect } from '../interface/defect';
@@ -8,17 +9,26 @@ import { Defect } from '../interface/defect';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnDestroy {
   columnTitles: string[];
   dataSource = new MatTableDataSource<Defect>();
+  private defectSubscription: Subscription;
 
   constructor(private mockDataService: MockDataService) {
     this.columnTitles = ['i', 'x', 'y'];
     this.dataSource.data = Array.from(this.mockDataService.getDefects().values());
     // console.log(this.dataSource);
+
+    this.defectSubscription = this.mockDataService.$selectedDefectObservable.subscribe((defect: Defect) => {
+      console.log(defect);
+    });
   }
 
   ngOnInit(): void {
     
+  }
+
+  ngOnDestroy(): void {
+    this.defectSubscription.unsubscribe();
   }
 }
