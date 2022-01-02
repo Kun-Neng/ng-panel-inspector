5912b39bab8e8ee8323e606426fa27aa8231cadb
Kun-Neng <kunneng.hung@gmail.com>
2022-01-02 10:23:42 +0800

    import formfield and input modules; create input fields to bind the panel properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MainComponent } from './main/main.component';
 import { TableComponent } from './table/table.component';
@@ -33,6 +35,8 @@ import { PanelSetupDialogComponent } from './panel-setup-dialog/panel-setup-dial
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
     FormsModule
   ],
   providers: [],
